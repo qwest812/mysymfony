aa02395c50cab7319a51ecbb51b1fb798d09d148
iaroslav.rybachuk <qwest812@yandex.ru>
2018-06-26 12:03:28 +0300

    work with sql

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Article;
 
 class ArticleController extends Controller
 {
@@ -14,7 +15,8 @@ class ArticleController extends Controller
     /**
      * single article page by id
      *
-     * @Route("/articles", name="article_page")
+     * @Route("/", name="article_index")
+     * @Template()
      */
     public function indexAction()
     {
@@ -26,19 +28,44 @@ class ArticleController extends Controller
 //            'name_in_twig'=>$name,
 //        ]);
 //        return new Response("<html><body>Article page:{$id}</body></html>");
-        return $this->render('article/index.html.twig');
+//        return $this->render('article/index.html.twig');
+
+        $repo=$this->get('doctrine')->getRepository('AppBundle:Article');
+        $articles=$repo->findAll();
+        dump($articles);
+        return ['articles'=>compact($articles)];
     }
 
 
     /**
      * single article page by id
      *
-     * @Route("/articlenew/{id}{sl}", name="articlenew_page", requirements={"id"="[1-9][0-9]*", "sl"="/?"})
+     * @Route("/show/{id}{sl}", name="article_show", requirements={"id"="[1-9][0-9]*", "sl"="/?"})
      */
-    public function articlenewAction(Request $request)
+    public function showAction(Request $request)
     {
         $id= $request->get('id');
         return $this->render('article/show.html.twig',['id_for_twig'=>$id]);
 //        return new Response("<html><body>Article page:{$id}</body></html>");
+    }
+    /**
+     * single article page by id
+     *
+     *
+     * @Route("/test", name="article_test")
+     * @Template()
+     */
+    public function testAction(){
+
+        $article =new Article();
+        $article->setTitle('Symfony start')->setContent('Some <b>text</b> bla bla');
+//        вызываем доктрину
+        $em=$this->get('doctrine')->getManager();
+
+//        подшлтовка для сохранения
+        $em->persist($article);
+        $em->flush();
+        dump($em);
+        return ['article'=>$article];
     }
 }
